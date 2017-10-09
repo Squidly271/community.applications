446897b3bd2fe4285262f3f096af55bf9983372d
Squidly271 <azawadzki@arrowgames.com>
2017-10-08 22:09:30 -0400

    Display global repository moderation within stats display

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/showModeration.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/showModeration.php
@@ -1,10 +1,20 @@
 <?PHP
 require_once("/usr/local/emhttp/plugins/community.applications/include/paths.php");
 $moderation = @file_get_contents($communityPaths['moderation']);
+$repositories = json_decode(file_get_contents($communityPaths['Repositories']),true);
 echo "<body bgcolor='white'>";
+foreach ($repositories as $repo) {
+	if ($repo['RepoComment']) {
+		$repoComment .= "<tr><td>{$repo['name']}</td><td>{$repo['RepoComment']}</td></tr>";
+	}
+}
+if ( $repoComment ) {
+	echo "<br><center><strong>Global Repository Comments:</strong></center><br><br><tt><table>$repoComment</table><br><br>";
+}
 if ( ! $moderation ) {
   echo "<br><br><center><b>No moderation entries found</b></center>";
 }
+echo "</tt><center><strong>Individual Application Moderation</strong></center><br><br>";
 $moderation = str_replace(" ","&nbsp;",$moderation);
 $moderation = str_replace("\n","<br>",$moderation);
 echo "<tt>$moderation";
