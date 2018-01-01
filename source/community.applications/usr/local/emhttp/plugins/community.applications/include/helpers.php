801808beb999bed7914fba02c70e458adcf23780
Squidly271 <azawadzki@arrowgames.com>
2017-12-31 22:38:15 -0500

    Fix case where if No Date, but date installed present but empty errors appear

--- source/community.applications/usr/local/emhttp/plugins/community.applications/include/helpers.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/include/helpers.php
@@ -312,7 +312,7 @@ function fixTemplates($template) {
 	$template['DonateImg'] = validURL($template['DonateImg']);
 	$template['DonateText'] = str_replace("'","&#39;",$template['DonateText']);
 	$template['DonateText'] = str_replace('"','&quot;',$template['DonateText']);
-  $template['Date'] = $template['Date'] ?: $template['DateInstalled'];
+  $template['Date'] = $template['Date'] ?: (is_numeric($template['DateInstalled'])) ?: 0;
 	
 	# support v6.2 redefining deprecating the <Beta> tag and moving it to a category
 	if ( stripos($template['Category'],":Beta") ) {
