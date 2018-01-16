529c55baf574942906b2a627d3afce085f6ee057
Squidly271 <azawadzki@arrowgames.com>
2018-01-16 17:03:49 -0500

    redo

--- source/community.applications/usr/local/emhttp/plugins/community.applications/include/caCredits.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/include/caCredits.php
@@ -1,7 +1,7 @@
 <?
 ###############################################################
 #                                                             #
-# Community Applications copyright 2015-2017, Andrew Zawadzki #
+# Community Applications copyright 2015-2018, Andrew Zawadzki #
 #                                                             #
 ###############################################################
 
@@ -30,7 +30,7 @@ $caCredits = "
         <td><font color='coral'>Main Development</font></td>
       </tr>
       <tr>
-        <td><img src='https://forums.lime-technology.com/uploads/monthly_2017_02/lime_wire.thumb.png.25ea8d93427793d74b1811b152f5aa34.png' width='48px'; height='48px;'></td>
+        <td><img src='https://forums.lime-technology.com/uploads/monthly_2018_02/lime_wire.thumb.png.25ea8d93427793d74b1811b152f5aa34.png' width='48px'; height='48px;'></td>
         <td><strong><font color='coral'>bonienl</font></strong></td>
         <td><font color='coral'>Additional Contributions</font></td>
       </tr>
@@ -45,13 +45,13 @@ $caCredits = "
         <td><font color='coral'>Additional Testing</font></td>
       </tr>
       <tr>
-        <td><img src='https://forums.lime-technology.com/uploads/monthly_2017_09/59cbbf15696ec_spaceinvaderlogo.png.aacf1828fe685979ad8ea1586bfca22f.png' width='48px' height='48px'></td>
+        <td><img src='https://forums.lime-technology.com/uploads/monthly_2018_09/59cbbf15696ec_spaceinvaderlogo.png.aacf1828fe685979ad8ea1586bfca22f.png' width='48px' height='48px'></td>
         <td><strong><font color='coral'>gridrunner</font></strong></td>
         <td><font color='coral'>Additional Testing</font></td>
       </tr>
     </table></center>
     <br>
-    <center><em><font size='1'>Copyright &copy; 2015-2017 Andrew Zawadzki</font></em></center>
+    <center><em><font size='1'>Copyright &copy; 2015-2018 Andrew Zawadzki</font></em></center>
     <center><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7M7CBCVU732XG' target='_blank'><img src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'></a></center>
     <br><center><a href='http://lime-technology.com/forum/index.php?topic=40262.0' target='_blank'>Plugin Support Thread</a></center>
   ";
