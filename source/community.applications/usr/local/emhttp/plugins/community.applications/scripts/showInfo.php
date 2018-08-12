6fec9ac94fb833b5c70bfa5d27ec0efff2cbf351
Squidly271 <azawadzki@arrowgames.com>
2018-08-12 09:23:42 -0400

    Add ability to display CA changelog from any screen

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/showInfo.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/showInfo.php
@@ -11,11 +11,10 @@ require_once 'webGui/include/Markdown.php';
 
 $app = urldecode($_POST['app']);
 $name = urldecode($_POST['name']);
-if ( ! $app ) {
-	echo "Something went wrong";
-}
 
-$changes = Markdown(shell_exec("/usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin changes /tmp/plugins/$app"));
+$path = $app ? "/tmp/plugins/$app" : "/var/log/plugins/community.applications.plg";
+
+$changes = Markdown(shell_exec("/usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin changes $path"));
 if ( $app == "community.applications.plg" ) {
 	echo "<div style='overflow:scroll; max-height:550px; height:600px; overflow-x:hidden; overflow-y:hidden;'><center><img height='60px' src='/plugins/community.applications/images/community.applications.png'><br><font size='3' color='white'>Community Applications Updated Change Log</font><br></center>";
 	echo "<center><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7M7CBCVU732XG' target='_blank'><img src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif' height='20px'></a></center>";
