2c90778580276abc99cd1af0e1df43c2aa3a465b
Squidly271 <azawadzki@arrowgames.com>
2018-08-28 10:29:54 -0400

    slight simplification on code for manual

--- source/community.applications/usr/local/emhttp/plugins/community.applications/include/manualExec.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/include/manualExec.php
@@ -1,17 +1,11 @@
 <?PHP
-function download_url($url, $path = "", $bg = false){
-	exec("curl --compressed --max-time 60 --silent --insecure --location --fail ".($path ? " -o '$path' " : "")." $url ".($bg ? ">/dev/null 2>&1 &" : "2>/dev/null"), $out, $exit_code );
-	return ($exit_code === 0 ) ? implode("\n", $out) : false;
-}
-
-function randomFile() {
-	return tempnam("/tmp","CA-Temp-");
-}
+require_once("/usr/local/emhttp/plugins/community.applications/include/helpers.php");
+require_once("/usr/local/emhttp/plugins/community.applications/include/paths.php");
 
 $filename = randomFile();
 download_url("https://raw.githubusercontent.com/Squidly271/ca.documentation/master/caDocumentation.html",$filename);
 $manual = @file_get_contents($filename);
-$manual = $manual ? $manual : "Unable to download manual.  Try again later";
+$manual = $manual ?: "Unable to download manual.  Try again later";
 @unlink($filename);
 echo $manual;
 ?>
