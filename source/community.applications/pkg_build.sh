2234132a33a689d2ddc81d716d7bc75e55897509
Squidly271 <azawadzki@arrowgames.com>
2018-10-16 09:40:25 -0400

    update plg but do not commit yet

--- source/community.applications/pkg_build.sh
+++ source/community.applications/pkg_build.sh
@@ -9,8 +9,8 @@ mkdir -p $tmpdir
 
 cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
 cd $tmpdir
-makepkg -l y -c y ${archive}/${plugin}-${version}.txz
+makepkg -l y -c y ${archive}/${plugin}-${version}-x86_64-1.txz
 rm -rf $tmpdir
 echo "MD5:"
-md5sum ${archive}/${plugin}-${version}.txz
+md5sum ${archive}/${plugin}-${version}-x86_64-1.txz
 
