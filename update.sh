2c673e38cc9ba55821e3d48e83c9c38bb2cc7fc9
Andrew Z <unraidsquid@gmail.com>
2019-01-01 18:20:20 -0500

    this is a test

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Enter commit comment: "
-read $commit
+read commit
+echo $commit
 git config --global user.email "unraidsquid@gmail.com"
 git config --global user.name "Andrew Z"
 git add *
-git commit -m '$commit'
+git commit -m "$commit"
 git push -f
