4816669d8d634c63d52b2b085c07f11ff92e122c
Squidly271 <azawadzki@arrowgames.com>
2019-01-19 17:59:23 -0500

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Enter commit comment: "
-read commit
-echo $commit
-git config --global user.email "unraidsquid@gmail.com"
-git config --global user.name "Andrew Z"
-git add *
-git commit -m "$commit"
-git push -f
