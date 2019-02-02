f7d18d1b2607e65667c50c7257ee88fed54752cb
Squidly271 <azawadzki@arrowgames.com>
2019-02-02 16:14:44 -0500

    continue

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -129,7 +129,7 @@ function my_display_apps($file,$pageNumber=1,$officialFlag=false,$selectedApps=f
 			$template['DonateText'] = "Donate To Author";
 		}
 		$template['display_DonateLink'] = $template['DonateLink'] ? "<a class='ca_tooltip donateLink' href='".$template['DonateLink']."' target='_blank' title='".$template['DonateText']."'>Donate To Author</a></font>" : "";
-		$template['display_DonateImage'] = $template['DonateLink'] ? "<a class='ca_tooltip donateLink' href='".$template['DonateLink']."' target='_blank' title='".$template['DonateText']."'><img src='https://github.com/Squidly271/community.applications/raw/master/webImages/donate-button-small.png' style='height:1.8rem;'></a></font>" : "";
+  	$template['display_DonateImage'] = $template['DonateLink'] ? "<a class='ca_tooltip donateLink donate' href='".$template['DonateLink']."' target='_blank' title='".$template['DonateText']."'>Donate</a></font>" : "";
 
 		$template['display_Project'] = $template['Project'] ? "<a class='ca_tooltip projectLink' target='_blank' title='Click to go the the Project Home Page' href='".$template['Project']."'></a>" : "";
 		$template['display_Support'] = $template['Support'] ? "<a class='ca_tooltip supportLink' href='".$template['Support']."' target='_blank' title='Click to go to the support thread'></a>" : "";
