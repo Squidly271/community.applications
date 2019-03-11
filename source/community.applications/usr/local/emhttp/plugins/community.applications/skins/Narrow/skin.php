d8a56ce45672da1315c9e466da86093ee1597485
Squidly271 <azawadzki@arrowgames.com>
2019-03-11 14:34:28 -0400

    Remove ability to search dockerhub by simply clicking on a template's name

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -275,7 +275,7 @@ function my_display_apps($file,$pageNumber=1,$officialFlag=false,$selectedApps=f
       }
     }
 		
-    $template['display_dockerName'] = ( $communitySettings['dockerSearch'] == "yes" && ! $template['Plugin'] ) ? "<a class='ca_tooltip ca_applicationName' data-appNumber='$ID' style='cursor:pointer' onclick='mySearch(this.innerText);' title='Search dockerHub for similar containers'>".$template['Name']."</a>" : "<span class='ca_applicationName'>{$template['Name']}</span>";
+    $template['display_dockerName'] = "<span class='ca_applicationName'>{$template['Name']}</span>";
     $template['Category'] = ($template['Category'] == "UNCATEGORIZED") ? "Uncategorized" : $template['Category'];
 
     if ( ( $template['Beta'] == "true" ) ) {
