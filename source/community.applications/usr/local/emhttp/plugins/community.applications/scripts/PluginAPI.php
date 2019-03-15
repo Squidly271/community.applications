3ff61e228e032af62f5745a61b0058afac0452f9
Squidly271 <azawadzki@arrowgames.com>
2019-03-14 21:00:06 -0400

    switch to strcmp

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/PluginAPI.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/PluginAPI.php
@@ -16,10 +16,10 @@ $url = @plugin("pluginURL","/boot/config/plugins/$plugin");
 download_url($url,"/tmp/plugins/$plugin");
 
 $changes = @plugin("changes","/tmp/plugins/$plugin");
-$version = trim(@plugin("version","/tmp/plugins/$plugin"));
+$version = @plugin("version","/tmp/plugins/$plugin");
 $min = @plugin("min","/tmp/plugins/$plugin") ?: "6.4.0";
 file_put_contents("/tmp/plugins/".pathinfo($plugin, PATHINFO_FILENAME).".txt",$changes);
-if ( $version > @plugin("version","/boot/config/plugins/$plugin") ) {
+if ( strcmp($version,@plugin("version","/boot/config/plugins/$plugin")) > 0 ) {
 	$unraid = parse_ini_file($communityPaths['unRaidVersion']);
 	if ( version_compare($min,$unraid['version'],">") ) {
 		$update = false;
@@ -29,7 +29,6 @@ if ( $version > @plugin("version","/boot/config/plugins/$plugin") ) {
 } else {
 	$update = false;
 }
-$output = json_encode(array("updateAvailable" => $update,"version" => $version,"min"=>$min,"changes"=>$changes));
-file_put_contents("/tmp/blah",$output);
-echo $output;
+echo json_encode(array("updateAvailable" => $update,"version" => $version,"min"=>$min,"changes"=>$changes));
+
 ?>
