44719d1c5781591e37f8f893f4a37331e69d74a0
Squidly271 <azawadzki@arrowgames.com>
2019-03-14 23:24:57 -0400

    Remove extra check for update on API

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/installPluginUpdate.sh
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/installPluginUpdate.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "Checking for upgrade";
-/usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin check $1
-echo "Installing update";
 /usr/local/emhttp/plugins/dynamix.plugin.manager/scripts/plugin update $1
-
