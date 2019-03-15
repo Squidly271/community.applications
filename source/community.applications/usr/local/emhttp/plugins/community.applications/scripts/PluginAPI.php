ca643daa6a3bdd0b548cd4f94f24fba21bed77af
Squidly271 <azawadzki@arrowgames.com>
2019-03-14 20:58:09 -0400

    fix

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/PluginAPI.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/PluginAPI.php
@@ -16,9 +16,8 @@ $url = @plugin("pluginURL","/boot/config/plugins/$plugin");
 download_url($url,"/tmp/plugins/$plugin");
 
 $changes = @plugin("changes","/tmp/plugins/$plugin");
-$version = @plugin("version","/tmp/plugins/$plugin");
+$version = trim(@plugin("version","/tmp/plugins/$plugin"));
 $min = @plugin("min","/tmp/plugins/$plugin") ?: "6.4.0";
-
 file_put_contents("/tmp/plugins/".pathinfo($plugin, PATHINFO_FILENAME).".txt",$changes);
 if ( $version > @plugin("version","/boot/config/plugins/$plugin") ) {
 	$unraid = parse_ini_file($communityPaths['unRaidVersion']);
@@ -30,5 +29,7 @@ if ( $version > @plugin("version","/boot/config/plugins/$plugin") ) {
 } else {
 	$update = false;
 }
-echo json_encode(array("updateAvailable" => $update,"version"=>$version,"min"=>$min,"changes"=>$changes));
+$output = json_encode(array("updateAvailable" => $update,"version" => $version,"min"=>$min,"changes"=>$changes));
+file_put_contents("/tmp/blah",$output);
+echo $output;
 ?>
