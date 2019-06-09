c8f93fa904c60409911b6fe18c2e946c15fbaf79
Squidly271 <azawadzki@arrowgames.com>
2019-06-09 12:20:37 -0400

    Remove easter egg

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -132,7 +132,7 @@ function my_display_apps($file,$pageNumber=1,$selectedApps=false,$startup=false)
 		if ($template['Blacklist']) {
 			unset($template['display_pinButton']);
 		}
-		if ( $template['Uninstall'] ) {
+		if ( $template['Uninstall'] && $template['Name'] != "Community Applications" ) {
 			$template['display_Uninstall'] = "<a class='ca_tooltip ca_fa-delete' title='Uninstall Application' ";
 			$template['display_Uninstall'] .= ( $template['Plugin'] ) ? "onclick='uninstallApp(&quot;".$template['MyPath']."&quot;,&quot;".$template['Name']."&quot;);'>" : "onclick='uninstallDocker(&quot;".$template['MyPath']."&quot;,&quot;".$template['Name']."&quot;);'>";
 			$template['display_Uninstall'] .= "</a>";
