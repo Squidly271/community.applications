a5a8b9a01fd913aeda2ec583f2397cb57b18246d
Squidly271 <azawadzki@arrowgames.com>
2020-05-26 03:50:56 -0400

    update

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -549,6 +549,7 @@ function getPopupDescription($appNumber) {
 		}
 	}
 
+
 	$unraidVersion = parse_ini_file($caPaths['unRaidVersion']);
 	$templateDescription .= ( $template['MinVer'] > "6.4.0" ) ? "<tr><td nowrap>".tr("Minimum OS:")."</td><td>Unraid v".$template['MinVer']."</td></tr>" : "";
 
