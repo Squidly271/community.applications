65ea47e235ac98f07f22489b4963ae6979db7f34
bergware <bergware@users.noreply.github.com>
2020-06-08 22:44:50 +0200

    Update updatePluginSupport.php

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/updatePluginSupport.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/updatePluginSupport.php
@@ -22,14 +22,6 @@ require_once "$docroot/plugins/dynamix/include/Wrappers.php";
 
 $dynamix = parse_plugin_cfg("dynamix");
 
-if ( $translations ) {
-	$pluginTranslations = @parse_lang_file("$docroot/languages/{$dynamix['locale']}/apps.txt");
-	$genericTranslations = @parse_lang_file("$docroot/languages/{$dynamix['locale']}/translations.txt");
-	$language = array_merge(is_array($genericTranslations) ? $genericTranslations : [],is_array($pluginTranslations) ? $pluginTranslations : [] );
-	if ( empty($language) ) 
-		$translations = false;
-}
-
 $plugins = glob("/boot/config/plugins/*.plg");
 $templates = readJsonFile($caPaths['community-templates-info']);
 if ( ! $templates ) {
