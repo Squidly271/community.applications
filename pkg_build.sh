dfb1a4868e56b3a9ea210fcc43e6be6e486d1e33
Andrew Z <unraidsquid@gmail.com>
2020-11-29 15:24:37 -0500

    Cleanup

---
+++ pkg_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+tmpdir=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
+archive="/mnt/disks/WINDOWS10_GitHub/community.applications/archive"
+
+version=$(date +"%Y.%m.%d")$1
+
+mkdir -p $tmpdir
+
+cd /tmp/GitHub/SourceCode/community.applications/source/community.applications/
+cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
+cd $tmpdir
+makepkg -l y -c y /tmp/GitHub/community.applications/archive/community.applications-${version}-x86_64-1.txz
+#rm -rf $tmpdir
+echo "MD5:"
+md5sum /tmp/GitHub/community.applications/archive/community.applications-${version}-x86_64-1.txz
+
