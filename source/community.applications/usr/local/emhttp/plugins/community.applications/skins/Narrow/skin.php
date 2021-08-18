a1ae5eb773ed59f3a9a29baa7a61e8cc452f94a7
Squidly271 <unraidsquid@gmail.com>
2021-08-18 11:07:19 -0400

    Fix update status on official containers

--- source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/skins/Narrow/skin.php
@@ -138,6 +138,7 @@ function my_display_apps($file,$pageNumber=1,$selectedApps=false,$startup=false)
 			$ID = $template['ID'];
 			
 			$tmpRepo = strpos($template['Repository'],":") ? $template['Repository'] : $template['Repository'].":latest";
+			$tmpRepo = strpos($tmpRepo,"/") ? $tmpRepo : "library/$tmpRepo";
 			if ( ! filter_var($dockerUpdateStatus[$tmpRepo]['status'],FILTER_VALIDATE_BOOLEAN) ) {
 				$template['UpdateAvailable'] = true;
 			} else { 
@@ -639,6 +640,7 @@ function getPopupDescriptionSkin($appNumber) {
 				if ( $caSettings['dockerRunning'] ) {
 					if ( $selected ) {
 						$tmpRepo = strpos($template['Repository'],":") ? $template['Repository'] : $template['Repository'].":latest";
+						$tmpRepo = strpos($tmpRepo,"/") ? $tmpRepo : "library/$tmpRepo";
 						if ( ! filter_var($dockerUpdateStatus[$tmpRepo]['status'],FILTER_VALIDATE_BOOLEAN) ) {
 							$installLine .= "<div><a class='appIconsPopUp ca_fa-update dockerUpdate' data-name='$name'> ".tr("Update")."</a></div>";
 						}
