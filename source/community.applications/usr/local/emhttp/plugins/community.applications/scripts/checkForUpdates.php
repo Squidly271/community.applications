c28dfddeac3859a6d6e5fedb27d6e6eb1749ee3c
Squidly271 <unraidsquid@gmail.com>
2022-06-05 16:06:33 -0400

    Added: check for updates button

--- source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/checkForUpdates.php
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/checkForUpdates.php
@@ -7,30 +7,33 @@
 #                                                             #
 ###############################################################
 $docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: "/usr/local/emhttp";
-echo $docroot;
+
+$_SERVER['REQUEST_URI'] = "docker/apps";
+@require_once "$docroot/plugins/dynamix/include/Translations.php";
 require_once "$docroot/plugins/community.applications/include/paths.php";
+require_once "$docroot/plugins/community.applications/include/helpers.php";
 
 if ( is_file($caPaths['updateRunning']) && file_exists("/proc/".@file_get_contents($caPaths['updateRunning'])) ) {
-	echo "Check for updates already running\n";
+	echo tr("Check for updates already running")."\n";
 	exit();
 }
 
 file_put_contents($caPaths['updateRunning'],getmypid());
 
 $updateFile = is_file("$docroot/plugins/dynamix.docker.manager/scripts/dockerupdate") ? "dockerupdate" : "dockerupdate.php";
-echo "Checking for docker container updates\n";
+echo tr("Checking for docker container updates")."\n";
 exec("$docroot/plugins/dynamix.docker.manager/scripts/dockerupdate check nonotify > /dev/null 2>&1");
-echo "Checking for plugin updates\n";
+echo tr("Checking for plugin updates")."\n";
 foreach (glob("/var/log/plugins/*.plg") as $plg) {
 	if ( $plg == "/var/log/plugins/community.applications.plg" || $plg == "unRAIDServer.plg" || $plg == "gui.search.plg" || $plg == "page.notes.plg")
 		continue; // avoid possible race condition since CA / gui.search automatically check for updates for themselves when on Apps tab
-	echo "Checking $plg\n";
+	echo sprintf(tr("Checking %s"),$plg)."\n";
 	exec("$docroot/plugins/dynamix.plugin.manager/scripts/plugin check ".escapeshellarg(basename($plg))." > /dev/null 2>&1");
 }
-echo "Checking for language updates\n";
+echo tr("Checking for language updates")."\n";
 foreach (glob("/var/log/plugins/lang-*.xml") as $lang) {
 	$lingo = str_replace(["lang-",".xml"],["",""],$lang);
-	echo "Checking ".basename($lingo)."\n";
+	echo sprintf(tr("Checking %s"),basename($lingo))."\n";
 	exec("$docroot/plugins/dynamix.plugin.manager/scripts/language check ".basename($lingo));
 }
 @unlink($caPaths['updateRunning']);
---
+++ source/community.applications/usr/local/emhttp/plugins/community.applications/scripts/checkForUpdates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/emhttp/plugins/community.applications/scripts/checkForUpdates.php
+
